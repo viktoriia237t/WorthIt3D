f3be37e3424f5e313d17636d08cb4d4af6a31357
@viktori237t <viktori237t@gmail.com>
2026-01-07 15:14:57 +0200

    Updates

--- assets/index-Bjr5OgRY.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-h8itIEGY.js";import"./index-BWIhF5zg.js";var d=a;export{d as default};
--- assets/src-UW24ZMRV-Cu5W3FUW.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-h8itIEGY.js";import"./index-BWIhF5zg.js";var t=a;export{t as default};
